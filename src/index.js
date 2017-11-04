b1ddf1e30122b561e9fe5a1b47af7303c219bf65
wzulfikar_mba <me@wzulfikar.com>
2017-11-04 21:01:55 +0800

    installed apollo client with graphcool endpoint

--- src/index.js
+++ src/index.js
@@ -4,5 +4,23 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+// create network interface for our endpoint (from react-apollo)
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj9lb8x9751my01218plgt27n'
+})
+
+// instantiate the ApolloClient
+const client = new ApolloClient({
+  networkInterface
+})
+
+ReactDOM.render(
+    // wrap the app with higher order component `ApolloProvider`
+	<ApolloProvider client={client}>
+		<App />
+	</ApolloProvider>, 
+	document.getElementById('root')
+);
 registerServiceWorker();

3959a8231963b434c3af403ce462a1e1d495c090
wzulfikar_mba <me@wzulfikar.com>
2017-11-05 00:41:41 +0800

    added LinkList with mock data

---
+++ src/components/LinkList.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Link from './Link'
+
+class LinkList extends React.Component {
+	render () {
+		const linksToRender = [{
+		      id: '1',
+		      description: 'The Coolest GraphQL Backend 😎',
+		      url: 'https://www.graph.cool'
+		    }, {
+		      id: '2',
+		      description: 'The Best GraphQL Client',
+		      url: 'http://dev.apollodata.com/'
+		    }]
+		return (
+		  <div>
+		  	{linksToRender.map(link => (
+		  		<Link key={link.id} link={link}/>	
+		  	))}
+		  </div>
+		)
+	}
+}
+
+export default LinkList
--- src/index.js
+++ src/index.js
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
 
-// create network interface for our endpoint (from react-apollo)
-const networkInterface = createNetworkInterface({
-  uri: 'https://api.graph.cool/simple/v1/cj9lb8x9751my01218plgt27n'
-})
-
-// instantiate the ApolloClient
 const client = new ApolloClient({
-  networkInterface
-})
+  networkInterface: createNetworkInterface({
+    uri: 'https://api.graph.cool/simple/v1/cj9lb8x9751my01218plgt27n',
+  }),
+});
 
 ReactDOM.render(
     // wrap the app with higher order component `ApolloProvider`

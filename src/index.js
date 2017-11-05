79b4a969f8d61dfe330fd3089a0be0ddae2fe18b
wzulfikar_mba <me@wzulfikar.com>
2017-11-05 19:50:08 +0800

    added apollo middleware for auth (using apollo-client 2.0)

--- src/index.js
+++ src/index.js
@@ -7,12 +7,35 @@ import registerServiceWorker from './registerServiceWorker';
 
 // apollo imports
 import ApolloClient from 'apollo-client';
-import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { ApolloProvider } from 'react-apollo';
 
+import { GC_AUTH_TOKEN } from './constants';
+
+const httpLink = createHttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9lb8x9751my01218plgt27n' })
+const middlewareLink = new ApolloLink((operation, forward) => {
+	if (!operation.options || !operation.options.headers) {
+	  if (!operation.options) {
+	  	operation.options = {}
+	  }
+	  operation.options.headers = {}
+	}
+	const token = localStorage.getItem(GC_AUTH_TOKEN)
+	operation.setContext({
+		headers: {
+			authorization: token ? `Bearer ${token}` : null
+		}
+	})
+	return forward(operation)
+})
+
+// construct `link` for use with ApolloClient's `link` 
+const link = middlewareLink.concat(httpLink)
+
 const client = new ApolloClient({
-	link: new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cj9lb8x9751my01218plgt27n' }),
+	link: link,
 	cache: new InMemoryCache().restore(window.__APOLLO_STATE__),
 });
 

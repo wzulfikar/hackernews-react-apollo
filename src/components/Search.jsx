3a17f8f574f895df1f2d536ed418f3ea5b472ffe
wzulfikar_mba <me@wzulfikar.com>
2017-11-06 02:04:11 +0800

    - added graphql query `AllLinksSearchQuery` - added loading indicator on link search - display notification if search text is empty

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,34 +1,81 @@
 import React, { Component } from 'react'
 
 import gql from 'graphql-tag'
-import { graphql, withApollo } from 'react-apollo'
+import {  withApollo } from 'react-apollo'
 
 import Link from './Link'
+import { noty } from '../utils'
 
 class Search extends Component {
 	state = {
 		links: [],
 		searchText: '',
+		loading: false,
 	}
 
 	render() {
 	  return (
 		<div>
 			<div>
-				Search
 				<input type="text" 
 				onChange={(e) => this.setState({ searchText: e.target.value })} 
-			/>
-			<button onClick={() => this._executeSearch() }>OK</button>
+				placeholder='insert text to search'
+				autofocus={true}
+				/>
+				<button onClick={() => this._executeSearch() }>{this.state.loading ? '...' : 'search'}</button>
 			</div>
-			{this.state.links.map((link, index) => { <Link key={link.id} link={link} index={index} />})}
+			{this.state.links.map((link, index) => <Link key={link.id} link={link} index={index} />)}
 		</div>
 	  )
 	}
 
 	_executeSearch = async () => {
-		// 
+		const { searchText } = this.state
+		if (!searchText.length) {
+			noty('Please insert text to search', 'error').show()
+			return
+		}
+		this.setState({ loading: true })
+
+		// send graphql query
+		const result = await this.props.client.query({
+			query: ALL_LINKS_SEARCH_QUERY,
+			variables: { searchText }
+		})
+		const links = result.data.allLinks
+
+		// update search results
+		this.setState({ links, loading: false })
 	}
 }
 
+const ALL_LINKS_SEARCH_QUERY = gql`
+query AllLinksSearchQuery($searchText: String!) {
+	allLinks(filter: {
+		OR: [{
+			url_contains: $searchText,
+		}, {
+			description_contains: $searchText,
+		}]
+	}) {
+		id
+		url
+		description
+		createdAt
+		postedBy {
+			id
+			name
+		}
+		votes {
+			id
+			user {
+				id
+			}
+		}
+	}
+}
+`
+
+// Inject instance of 'ApolloClient' as component's props (`props.client`)
+// by wrapping our Search component with `withApollo` function
 export default withApollo(Search)

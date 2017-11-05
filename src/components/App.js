57b690b06b379420a8c8a67bee21f566fad232ee
wzulfikar_mba <me@wzulfikar.com>
2017-11-05 16:50:40 +0800

    demonstrate graphql mutation via CreateLink component

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 // import logo from '../logo.svg';
 import '../styles/App.css';
 
-import LinkList from './LinkList'
+// import LinkList from './LinkList'
+import CreateLink from './CreateLink'
 
 class App extends Component {
   render() {
     return (
-      <LinkList />
+      <CreateLink />
     );
   }
 }

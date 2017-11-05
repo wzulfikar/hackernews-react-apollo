a010d3dee8f832ac46dded75fb22e2a42a1f6b1f
wzulfikar_mba <me@wzulfikar.com>
2017-11-05 19:19:48 +0800

    - added createUserMutation and signinUserMutation - redirect to `/` on login/signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 
+import { graphql, compose } from 'react-apollo'
+import gql from 'graphql-tag'
 import {GC_USER_ID, GC_AUTH_TOKEN} from '../constants'
 
 class Login extends Component {
@@ -48,7 +50,32 @@ class Login extends Component {
 	}
 
 	_confirm = async () => {
-		// 
+		const { name, email, password } = this.state
+		if (this.state.login) {
+			// login user
+			const result = await this.props.signinUserMutation({
+				variables: {
+					email,
+					password,
+				}
+			})
+			const id = result.data.signinUser.user.id
+			const token = result.data.signinUser.token
+			this._saveUserData(id, token)
+		} else {
+			// create user
+			const result = await this.props.createUserMutation({
+				variables: {
+					name,
+					email,
+					password,
+				}
+			})
+			const id = result.data.signinUser.user.id
+			const token = result.data.signinUser.token
+			this._saveUserData(id, token)
+		}
+		this.props.history.push('/')
 	}
 
 	_saveUserData = (id, token) => {
@@ -57,4 +84,49 @@ class Login extends Component {
 	}
 }
 
-export default Login
+const CREATE_USER_MUTATION = gql`
+mutation CreateUserMutation($name: String!, $email: String!, $password: String!) {
+  createUser(
+    name: $name,
+    authProvider: {
+      email: {
+        email: $email,
+        password: $password
+      }
+    }
+  ) {
+    id
+  }
+
+  signinUser(email: {
+    email: $email,
+    password: $password
+  }) {
+    token
+    user {
+      id
+    }
+  }
+}
+`
+
+const SIGNIN_USER_MUTATION = gql`
+mutation SigninUserMutation($email: String!, $password: String!) {
+  signinUser(email: {
+    email: $email,
+    password: $password
+  }) {
+    token
+    user {
+      id
+    }
+  }
+}
+`
+
+// use `compose` for the export statement 
+// since we've more than one mutation to wrap our component with.
+export default compose (
+	graphql(CREATE_USER_MUTATION, {name: 'createUserMutation'}),
+	graphql(SIGNIN_USER_MUTATION, {name: 'signinUserMutation'}),
+)(Login)

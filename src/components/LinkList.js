ce043de425baae2f37156745e48ddd533c3b8a0b
wzulfikar_mba <me@wzulfikar.com>
2017-11-05 23:54:42 +0800

    sync votes in store with server's respond once a link is voted

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -24,21 +24,45 @@ class LinkList extends React.Component {
 		
 		return (
 		  <div>
-		  	{linksToRender.map(link => (
-		  		<Link key={link.id} link={link}/>	
+		  	{linksToRender.map((link, idx) => (
+		  		<Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote} link={link} index={idx}/>
 		  	))}
 		  </div>
 		)
 	}
+
+	_updateCacheAfterVote = (store, createVote, linkId) => {
+		// 1. read `ALL_LINKS_QUERY` from cache using ApolloClient's `readQuery`
+		const data = store.readQuery({ query: ALL_LINKS_QUERY })
+
+		// 2. retrieve link that the user just voted
+		const votedLink = data.allLinks.find(link => link.id === linkId)
+
+		// 3. assign `votes` from server to votedLink's `votes`
+		votedLink.votes = createVote.link.votes
+
+		// 4. take the modified data and write it back into the store (cache)
+		store.writeQuery({ query: ALL_LINKS_QUERY, data })
+	}
 }
 
-const ALL_LINKS_QUERY = gql`
+export const ALL_LINKS_QUERY = gql`
 	query AllLinksQuery {
 		allLinks {
 			id
 			createdAt
 			url
 			description
+			postedBy {
+		      id
+		      name
+		    }
+		    votes {
+		      id
+		      user {
+		        id
+		      }
+		    }
 		}
 	}
 `

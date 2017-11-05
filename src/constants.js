3e1fab91a8f8cfff0a48c0297dad7c5b583c89f0
wzulfikar_mba <me@wzulfikar.com>
2017-11-05 19:51:46 +0800

    adjust constants vars

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 // adjust user id & token accordingly
-export const GC_USER_ID = 'your-graphcool-user-id'
-export const GC_AUTH_TOKEN = 'your-graphcool-auth-token'
+export const GC_USER_ID = 'graphcool-user-id'
+export const GC_AUTH_TOKEN = 'graphcool-auth-token'

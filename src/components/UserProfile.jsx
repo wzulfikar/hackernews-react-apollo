f031376296dc735f210bcb723afc33e5c72e9b79
wzulfikar_mba <me@wzulfikar.com>
2017-11-06 23:04:35 +0800

    UP: user profile page

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,43 +1,77 @@
 import React, { Component } from 'react'
 
+import gql from 'graphql-tag'
+import { graphql } from 'react-apollo'
+
 import { Link } from 'react-router-dom'
 import { timeDifferenceForDate } from '../utils'
 
 class UserProfile extends Component {
   username = this.props.match.params.username
   render() {
+  	if (this.props.userProfileQuery.loading) {
+  		return <div>Loading..</div>
+  	}
+
+  	if (!this.props.userProfileQuery.User) {
+  		return <div>user not found</div>
+  	}
+
+  	const user = this.props.userProfileQuery.User
+
     return (
       <div className="w85">
 		<table className='gray'>
+		<tbody>
 			<tr>
 				<td>user</td>
 				<td>:</td>
-				<td>{this.username}</td>
+				<td>{user.username}</td>
+			</tr>
+			<tr>
+				<td>name</td>
+				<td>:</td>
+				<td>{user.name}</td>
 			</tr>
 			<tr>
 				<td>created</td>
 				<td>:</td>
-				<td>{ timeDifferenceForDate(new Date()) }</td>
-			</tr>
-			<tr>
-				<td>karma</td>
-				<td>:</td>
-				<td>karma</td>
+				<td>{ timeDifferenceForDate(user.createdAt) }</td>
 			</tr>
 			<tr>
 				<td>about</td>
 				<td>:</td>
-				<td>karma</td>
+				<td>{user.bio || '(Not set)'}</td>
 			</tr>
+		</tbody>
 		</table>
 		<div className='mt2'>
-			<Link className='db black' to={`/u/${this.username}/submissions`}>submissions</Link>
-			<Link className='db black' to={`/u/${this.username}/comments`}>comments</Link>
-			<Link className='db black' to={`/u/${this.username}/favorites`}>favorites</Link>
+			<Link className='db black' to={`/u/${user.username}/submissions`}>submissions</Link>
+			<Link className='db black' to={`/u/${user.username}/comments`}>comments</Link>
+			<Link className='db black' to={`/u/${user.username}/favorites`}>favorites</Link>
 		</div>
       </div>
     );
   }
 }
 
-export default UserProfile;
+const USER_PROFILE_QUERY = gql`
+query userProfileQuery($username:String!) {
+	User(username:$username) {
+	    name
+	    username
+	    bio
+	    createdAt
+	}
+}
+`
+
+export default graphql(USER_PROFILE_QUERY, {
+	name: 'userProfileQuery',
+	options: (ownProps) => {
+	  const username = ownProps.match.params.username
+	  return {
+	    variables: { username }
+	  }
+	}
+})(UserProfile);
